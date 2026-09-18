2b67642f5df81dc1fea8ac818a2aa0dc30477960
MaGo1024 <olumideodusote@gmail.com>
2026-09-18 14:40:17 +0100

    Saving Progress.

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from tkinter import *
+
+import pandas
 import pandas as pd
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-
-data = pd.read_csv("./data/french_words.csv")
-to_learn = data.to_dict(orient="records")
 current_card = {}
+to_learn = {}
+try:
+    data = pandas.read_csv("./data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pd.read_csv("./data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
 
 def next_card():
     global current_card, flip_timer
@@ -23,7 +31,13 @@ def flip_card():
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
 
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pd.DataFrame(to_learn)
+    data.to_csv("./data/words_to_learn.csv", index=False)
 
+    next_card()
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Flash Cards")
@@ -45,7 +59,7 @@ unknown_button = Button(image=cross_image, highlightthickness=0, command=next_ca
 unknown_button.grid(row=1, column=0)
 
 checked_image = PhotoImage(file="./images/right.png")
-known_button = Button(image=checked_image, highlightthickness=0, command=next_card)
+known_button = Button(image=checked_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
